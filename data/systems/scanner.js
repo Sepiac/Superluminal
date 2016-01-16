3991a517337401d28b5096943babd00c1768808a
Dane Russell <dane.russell@availity.com>
2016-01-15 21:44:11 -0500

    Added scanner system.

---
+++ data/systems/scanner.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var scannerSystem = {};
+
+scannerSystem.name = 'scanner';
+scannerSystem.activated = false;
+
+scannerSystem.activate = function() {
+  scannerSystem.activated = true;
+  console.log('Scanner system activated.');
+};
+
+scannerSystem.shutdown = function() {
+  scannerSystem.activated = false;
+  console.log('Scanner system deactivated.');
+};
+
+module.exports = scannerSystem;
