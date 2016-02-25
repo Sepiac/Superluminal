5ce7718a7fc7e831e94c1991d383a930458064eb
Dane Russell <danerussell4@gmail.com>
2016-02-24 19:54:53 -0500

    issue #13: Added Troy's locations

--- DEFAULT_CAMPAIGN/locations/alphaCentauri.js
+++ DEFAULT_CAMPAIGN/locations/alphaCentauri.js
@@ -2,7 +2,7 @@
 var alphaCentauri = {};
 
 alphaCentauri.name = 'Alpha_Centauri';
-alphaCentauri.description = 'You are not home anymore.';
+alphaCentauri.description = 'A collection of small, green islands, Populated by horned brutes who value tradition and intuition above intellect. Somehow the inhabitants developed a keen sense of fashion, catered to aquatic leisure and exploration. They treat water like air, that resource that your planet needs so desperately.';
 
 alphaCentauri.adjacentSystems = [
   'Sol'
---
+++ DEFAULT_CAMPAIGN/locations/xantu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var xantu = {};
+
+xantu.name = 'xantu';
+xantu.description = 'All the land here flows like water. The insectoid inhabitants all seem to glide or fly rather than walk. Nothing grows on the glowing silver pools.';
+xantu.energy = 10;
+
+xantu.adjacentSystems = [
+  'Sol'
+];
+
+xantu.resources = {
+  energy: 5
+};
+
+module.exports = xantu;
